2207483700db9f15aee885427a593f5fd1fc71dc
efzr <emerson.zapatarivas@gmail.com>
2024-07-14 23:12:19 -0600

    http requests

--- Application/Application.DTO/UserDTO.cs
+++ Application/Application.DTO/UserDTO.cs
@@ -23,5 +23,4 @@ public class CreateUserDTO
     public string? NombreUsuario { get; set; }
     public string? Email { get; set; }
     public string? Password { get; set; }
-    public string? Token { get; set; }
 }
